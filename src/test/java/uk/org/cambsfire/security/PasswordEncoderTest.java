17f71e8e3845cdd15efe2757b8d0f605236bd35a
Dave Bower <dave@lourish.com>
2017-05-10 16:40:53 +0100

    Corrected package for test

--- src/test/java/uk/org/cambsfire/security/PasswordEncoderTest.java
+++ src/test/java/uk/org/cambsfire/security/PasswordEncoderTest.java
@@ -1,4 +1,4 @@
-package uk.org.cambsfire;
+package uk.org.cambsfire.security;
 
 /*-
  * #%L
@@ -8,18 +8,18 @@ package uk.org.cambsfire;
  * %%
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. Neither the name of the Cambridgeshire Fire and Rescue Service nor the names of its contributors
  *    may be used to endorse or promote products derived from this software without
  *    specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -33,13 +33,9 @@ package uk.org.cambsfire;
  * #L%
  */
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
-
-import uk.org.cambsfire.security.EncodedPassword;
-import uk.org.cambsfire.security.PasswordEncoder;
 
 public class PasswordEncoderTest {
 
